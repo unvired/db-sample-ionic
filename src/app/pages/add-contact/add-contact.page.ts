57122a87cb45973d9e562b05d7561f0d53f05d87
Pavithra <pavithra.vp@unvired.io>
2020-05-08 11:42:09 +0530

    Added platform cordova ios, on create success navigate back in add-contact.page.ts

--- src/app/pages/add-contact/add-contact.page.ts
+++ src/app/pages/add-contact/add-contact.page.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CONTACT_HEADER } from 'src/models/CONTACT_HEADER';
-import { AlertController , Platform} from '@ionic/angular';
-import { AppConstant } from '../../../constants/appConstants';
+import { AlertController, NavController} from '@ionic/angular';
 import { UnviredCordovaSDK, RequestType, ResultType } from '@ionic-native/unvired-cordova-sdk/ngx';
 import { HomePageService } from 'src/app/services/home-page.service';
-import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-add-contact',
@@ -19,64 +17,61 @@ export class AddContactPage implements OnInit {
               private alertController: AlertController,
               private unviredSDK: UnviredCordovaSDK,
               private homeService: HomePageService,
-              private platform: Platform,
-              private route: ActivatedRoute) { }       
+              private navCtrl: NavController,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(()=>{
+    this.route.paramMap.subscribe(() => {
       this.homeService.subscription.unsubscribe();
-    })
+    });
   }
 
-  goToHome(){
-    this.router.navigate(['/home']);
+  goToHome() {
+    this.navCtrl.pop();
   }
 
-  createContact(){
-      if (this.contactHeader.ContactName == undefined || this.contactHeader.ContactName.length == 0 || this.contactHeader.ContactName == "") {
-         this.showAlert("", "Enter name.")
-         return
+  createContact() {
+      // tslint:disable-next-line:max-line-length
+      if (this.contactHeader.ContactName === undefined || this.contactHeader.ContactName.length === 0 || this.contactHeader.ContactName === '') {
+         this.showAlert('', 'Enter name.');
+         return;
       }
 
-      if (this.contactHeader.Phone == undefined || this.contactHeader.Phone.length == 0 || this.contactHeader.Phone == "") {
-          this.showAlert("", "Enter Phone number.")
-          return
+      if (this.contactHeader.Phone === undefined || this.contactHeader.Phone.length === 0 || this.contactHeader.Phone === '') {
+          this.showAlert('', 'Enter Phone number.');
+          return;
       }
 
-      if (this.contactHeader.Email == undefined || this.contactHeader.Email.length == 0 || this.contactHeader.Email == undefined) {
-          this.showAlert("", "Enter Email.")
-          return
+      if (this.contactHeader.Email === undefined || this.contactHeader.Email.length === 0 || this.contactHeader.Email === undefined) {
+          this.showAlert('', 'Enter Email.');
+          return;
       }
-      this.sendDataToServer();     
+      this.sendDataToServer();
   }
 
-  async sendDataToServer(){
-    console.log("Sending data to server.....");
-    var inputHeader: any = {};
-    inputHeader["CONTACT_HEADER"] = this.contactHeader;
+  async sendDataToServer() {
+    console.log('Sending data to server.....');
+    const inputHeader: any = {};
+    inputHeader.CONTACT_HEADER = this.contactHeader;
     const data = { CONTACT : [inputHeader]};
-    console.log("INSERTING DATA TO SERVER");
-    let  result = await this.unviredSDK.syncForeground(RequestType.RQST, '', data, 'UNVIRED_DB_SAMPLE_PA_CREATE_CONTACT', true);
-    ///let  result = await this.unviredSDK.syncForeground(RequestType.RQST, '', data, 'UNVIRED_DB_SAMPLE_PA_CREATE_CONTACT', true);
-    console.log("***RESULT****", result);
-    if(result.type == ResultType.success){
-      this.showAlert("","Contact Created Successfully");
-    }else{
-      console.log("ERROR",result.error);
-      this.showAlert("",result.error);
+    console.log('INSERTING DATA TO SERVER');
+    const  result = await this.unviredSDK.syncForeground(RequestType.RQST, '', data, 'UNVIRED_DB_SAMPLE_PA_CREATE_CONTACT', true);
+    console.log('***RESULT****', JSON.stringify(result));
+    if (result.type === ResultType.success) {
+      this.showAlert('', 'Contact Created Successfully');
+    } else {
+      console.log('ERROR', result.error);
+      this.showAlert('', result.error);
     }
     this.contactHeader.ContactName = '';
     this.contactHeader.Phone = '';
     this.contactHeader.Email = '';
 
     this.homeService.sub.next('');
-    //this.router.navigate(['/home']);
-
-    // let resultSelect = this.unviredSDK.dbSelect(AppConstant.TABLE_NAME_CONTACT_HEADER, '');
-    // console.log("Result getting from CONTACT_HEADER---- ", resultSelect);
+    this.navCtrl.pop();
   }
 
-  async showAlert(title: string, message: string){
+  async showAlert(title: string, message: string) {
     const alert = await this.alertController.create({
       header: title,
       subHeader: message,
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -11,12 +11,11 @@ import { HomePageService } from 'src/app/services/home-page.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit {
   public contacts;
-
-  private conatctHeaders: CONTACT_HEADER[] = []
-  private searchResultDataSource: CONTACT_HEADER[] = []
-  private UIModels: UIModel[] = []
+  private conatctHeaders: CONTACT_HEADER[] = [];
+  private searchResultDataSource: CONTACT_HEADER[] = [];
+  UIModels: UIModel[] = [];
 
   constructor(private router: Router,
               private actionSheetCtrl: ActionSheetController,
@@ -24,40 +23,33 @@ export class HomePage implements OnInit{
               public alertController: AlertController,
               private unviredSDK: UnviredCordovaSDK,
               private homeService: HomePageService,
-              private route: ActivatedRoute) {
-                console.log("CALLING CONSTRUCTOR OF HOMEPAGE");
+              private route: ActivatedRoute) { }
 
-              }
-
-  ngOnInit()
-  {
-    this.route.paramMap.subscribe(()=>{
-      
-      this.homeService.subscription = this.homeService.getSubject().subscribe((data)=>{
-        console.log("Data >>",data);
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.homeService.subscription = this.homeService.getSubject().subscribe((data) => {
         this.displayContacts();
-      })
-    })
+      });
+    });
   }
 
-  displayContacts(){
-    console.log("Get All Conatct Headers from DB.....");
-    this.unviredSDK.dbSelect(AppConstant.TABLE_NAME_CONTACT_HEADER,'')
-    .then((result)=>{
-      console.log("@@@@",result.data);
-        if(result.type == ResultType.success){
+  displayContacts() {
+    console.log('Get All Conatct Headers from DB.....');
+    this.unviredSDK.dbSelect(AppConstant.TABLE_NAME_CONTACT_HEADER, '')
+    .then((result) => {
+      if (result.type === ResultType.success) {
           this.conatctHeaders = result.data;
-          this.sortContactHeader(this.conatctHeaders)
-          console.log("Conatct Headers from DB:" + this.conatctHeaders.length);
+          this.sortContactHeader(this.conatctHeaders);
+          console.log('Conatct Headers from DB:' + this.conatctHeaders.length);
         }
     })
-    .catch(error =>{
-       console.log(" Error while fetching the data from server...");
-    })
+    .catch(error => {
+       console.log(' Error while fetching the data from server...');
+    });
   }
 
   async menuButtonClicked() {
-    let actionSheet = await this.actionSheetCtrl.create({
+    const actionSheet = await this.actionSheetCtrl.create({
       buttons: [
         {
           text: 'Get Contacts',
@@ -73,11 +65,10 @@ export class HomePage implements OnInit{
           }
         }
       ]
-    })
-    actionSheet.present()
+    });
+    actionSheet.present();
   }
 
-  
     async presentAlert() {
       const alert = await this.alertController.create({
         header: 'Alert',
@@ -94,39 +85,40 @@ export class HomePage implements OnInit{
 
   sortContactHeader(contactHeaders: CONTACT_HEADER[]) {
     this.ngZone.run(() => {
-      let alphabet: any = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-      this.UIModels = []
-      for (var letter of alphabet) {
-        let letterInLowerCase = letter.toLowerCase;
-        let searchLetter = letter as string
+      const alphabet: any = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+      this.UIModels = [];
+      for (const letter of alphabet) {
+        const letterInLowerCase = letter.toLowerCase;
+        const searchLetter = letter as string;
 
-        var matches = contactHeaders.filter(contact => contact.ContactName.startsWith(searchLetter))
+        const matches = contactHeaders.filter(contact => contact.ContactName.startsWith(searchLetter));
         if (matches.length > 0) {
           this.ngZone.run(() => {
-            let uiModel = new UIModel()
-            uiModel.section = letter
-            matches.sort(function (a, b) { return (a.ContactName > b.ContactName) ? 1 : ((b.ContactName > a.ContactName) ? -1 : 0); });
-            uiModel.contactHeaders = matches as [CONTACT_HEADER]
-            this.UIModels.push(uiModel)
-          })
+            const uiModel = new UIModel();
+            uiModel.section = letter;
+            // tslint:disable-next-line:only-arrow-functions
+            matches.sort(function(a, b) { return (a.ContactName > b.ContactName) ? 1 : ((b.ContactName > a.ContactName) ? -1 : 0); });
+            uiModel.contactHeaders = matches as [CONTACT_HEADER];
+            this.UIModels.push(uiModel);
+          });
         }
       }
-    })
+    });
   }
 
   filterData(ev: any) {
     // Reset items back to all of the items
-    this.sortContactHeader(this.conatctHeaders)
+    this.sortContactHeader(this.conatctHeaders);
 
     // set val to the value of the searchbar
-    let val = ev.target.value;
+    const val = ev.target.value;
 
     // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
+    if (val && val.trim() !== '') {
       this.searchResultDataSource = this.conatctHeaders.filter((contact) => {
         return ((contact.ContactName.toLowerCase().indexOf(val.toLowerCase()) > -1));
-      })
-      this.sortContactHeader(this.searchResultDataSource)
+      });
+      this.sortContactHeader(this.searchResultDataSource);
     }
   }
 
